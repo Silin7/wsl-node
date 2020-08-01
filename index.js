96d01280b5e4112ac57667689b1951e05a0534eb
silin.wang <wsl2535620700@qq.com>
2020-08-01 17:22:36 +0800

    mysql

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const http = require("http")
-const url = require("url")
+var mysql = require('mysql');
 
 
 const app = http.createServer(function (req, res) {
-  console.log(req.url)
-  console.log()
   if (req.url !== "./favicon.ico") {
     res.writeHead(200,{
       "Content-Type": "text/html;charset=UTF-8"
     })
     // 
-    let urlmsg = url.parse(req.url, true)
-    console.log(urlmsg)
-    console.log(urlmsg.query.name)
-    console.log(urlmsg.query.password)
+    console.log('start')
+    var connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password : '123456',
+      port: '8088',
+      database: 'test_library' 
+    }); 
+    connection.connect();
+    var sql = 'SELECT * FROM `login_information` WHERE 1';
+    //查
+    connection.query(sql, function (err, result) {
+      if(err){
+        console.log('[SELECT ERROR] - ',err.message);
+        return;
+      }
+      console.log(result);
+      connection.end();
+    });
+    console.log('end')
     // 
     res.end(JSON.stringify({
       msg: 'success',
